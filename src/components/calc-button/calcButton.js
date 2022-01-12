801be527106ce879e6f9c790488de6757a936fec
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-12 14:34:44 +0600

    Add calcButton component

---
+++ src/components/calc-button/calcButton.js
@@ -0,0 +1,20 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import './calcButton.scss';
+
+const CalcButton = ({ name, primary, span2, handleClick, ...otherProps }) => (
+  <button
+    name={name}
+    onClick={handleClick}
+    type="button"
+    className={`calculator__button${
+      span2 ? ' calculator__button--span-2' : ''
+    }${primary ? ' calculator__button--primary' : ''}`}
+    {...otherProps}
+  >
+    {name}
+  </button>
+);
+
+export default CalcButton;
