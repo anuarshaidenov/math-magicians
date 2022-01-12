c20e6f3ffeae33190a40c84f75b7e9adeb382491
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-12 16:16:13 +0600

    Refactor with hooks

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,59 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 import './calculator.scss';
 
 import calculate from '../../logic/calculate';
 
 import CalcButton from '../calc-button/calcButton';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [calculatorValues, setCalculatorValues] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
-
-  handleClick = (e) => {
+  const handleClick = (e) => {
     const pressedButton = e.target.name;
-    const newState = calculate(this.state, pressedButton);
-    this.setState(newState);
+    const newState = calculate(calculatorValues, pressedButton);
+    setCalculatorValues(newState);
   };
 
-  render() {
-    const { total, next, operation } = this.state;
+  const { total, next, operation } = calculatorValues;
 
-    return (
-      <div className="calculator">
-        <div name="0" className="calculator__result">
-          {!total && !next && !operation
-            ? '0'
-            : `${total || ''} ${operation || ''} ${next || ''}`}
-        </div>
-        <CalcButton name="AC" handleClick={this.handleClick} />
-        <CalcButton name="+/-" handleClick={this.handleClick} />
-        <CalcButton name="%" handleClick={this.handleClick} />
-        <CalcButton name="÷" primary handleClick={this.handleClick} />
-        <CalcButton name="7" handleClick={this.handleClick} />
-        <CalcButton name="8" handleClick={this.handleClick} />
-        <CalcButton name="9" handleClick={this.handleClick} />
-        <CalcButton name="x" primary handleClick={this.handleClick} />
-        <CalcButton name="4" handleClick={this.handleClick} />
-        <CalcButton name="5" handleClick={this.handleClick} />
-        <CalcButton name="6" handleClick={this.handleClick} />
-        <CalcButton name="-" primary handleClick={this.handleClick} />
-        <CalcButton name="1" handleClick={this.handleClick} />
-        <CalcButton name="2" handleClick={this.handleClick} />
-        <CalcButton name="3" handleClick={this.handleClick} />
-        <CalcButton name="+" primary handleClick={this.handleClick} />
-        <CalcButton name="0" span2 handleClick={this.handleClick} />
-        <CalcButton name="." handleClick={this.handleClick} />
-        <CalcButton name="=" primary handleClick={this.handleClick} />
+  return (
+    <div className="calculator">
+      <div name="0" className="calculator__result">
+        {!total && !next && !operation
+          ? '0'
+          : `${total || ''} ${operation || ''} ${next || ''}`}
       </div>
-    );
-  }
-}
+      <CalcButton name="AC" handleClick={handleClick} />
+      <CalcButton name="+/-" handleClick={handleClick} />
+      <CalcButton name="%" handleClick={handleClick} />
+      <CalcButton name="÷" primary handleClick={handleClick} />
+      <CalcButton name="7" handleClick={handleClick} />
+      <CalcButton name="8" handleClick={handleClick} />
+      <CalcButton name="9" handleClick={handleClick} />
+      <CalcButton name="x" primary handleClick={handleClick} />
+      <CalcButton name="4" handleClick={handleClick} />
+      <CalcButton name="5" handleClick={handleClick} />
+      <CalcButton name="6" handleClick={handleClick} />
+      <CalcButton name="-" primary handleClick={handleClick} />
+      <CalcButton name="1" handleClick={handleClick} />
+      <CalcButton name="2" handleClick={handleClick} />
+      <CalcButton name="3" handleClick={handleClick} />
+      <CalcButton name="+" primary handleClick={handleClick} />
+      <CalcButton name="0" span2 handleClick={handleClick} />
+      <CalcButton name="." handleClick={handleClick} />
+      <CalcButton name="=" primary handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default Calculator;
