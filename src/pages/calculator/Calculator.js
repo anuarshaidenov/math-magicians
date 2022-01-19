a5e2ad6acad42b42c8942bfebf645b3427c82680
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-01-19 20:27:12 +0600

    Fix layout

--- src/pages/calculator/Calculator.js
+++ src/pages/calculator/Calculator.js
@@ -3,9 +3,13 @@ import CalculatorComponent from '../../components/calculator/calculator';
 import './calculator.scss';
 
 const Calculator = () => (
-  <div className="calculator-page page container container--small">
-    <h1 className="heading">Let&apos;s do some math!</h1>
-    <CalculatorComponent />
+  <div className="calculator-page page container">
+    <h1 className="heading calculator-page__heading">
+      Let&apos;s do some math!
+    </h1>
+    <div className="calculator-page__calculator">
+      <CalculatorComponent />
+    </div>
   </div>
 );
 
